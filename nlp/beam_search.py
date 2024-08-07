b226df77de751826a1bd7c27ba6ed10521cc7003
Victor Semionov <vsemionov@gmail.com>
2024-08-07 23:10:56 +0300

    correction

--- nlp/beam_search.py
+++ nlp/beam_search.py
@@ -76,7 +76,7 @@ def beam_search(
         if not branches:
             break
 
-    output = max(nodes, key=lambda node: (node.path[-1] == eos_class, node.score))
+    output = max(nodes, key=lambda node: (node.path[-1] == eos_class, node.score)).path
     if output[-1] == eos_class:
         output = output[:-1]
     return output
